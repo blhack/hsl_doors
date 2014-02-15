f49fbcf5cf3f0c4f313e0e31ad860adea76f0220
Ryan McDermott <ryan@Ryans-MacBook-Pro.local>
2014-02-15 16:37:32 -0700

    Changed file header

--- hsl_doors.ino
+++ hsl_doors.ino
@@ -1,25 +1,6 @@
 /*
-  Running shell commands using Process class. 
- 
- This sketch demonstrate how to run linux shell commands
- using an Arduino Yún. It runs the wifiCheck script on the linino side
- of the Yun, then uses grep to get just the signal strength line.
- Then it uses parseInt() to read the wifi signal strength as an integer,
- and finally uses that number to fade an LED using analogWrite().
- 
- The circuit:
- * Arduino Yun with LED connected to pin 9
- 
- created 12 Jun 2013
- by Cristian Maglie
- modified 25 June 2013
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://arduino.cc/en/Tutorial/ShellCommands
- 
- */
+ This sketch is heavily based on one by Cristian Maglie.
+*/
 
 #include <Process.h>
 #include <LiquidCrystal.h>
